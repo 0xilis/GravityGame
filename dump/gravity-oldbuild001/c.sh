f558267ced8415ac980811861f0113967eaa122c
0xilis <109871561+0xilis@users.noreply.github.com>
2024-03-31 17:49:28 -0500

    Delete dump directory

--- dump/gravity-oldbuild001/c.sh
+++
@@ -1,2 +0,0 @@
-# https://stackoverflow.com/questions/32468761/how-can-i-link-with-gcc-and-sdl-image-library - linking SDL2_image
-gcc main.c -o main -L/usr/local/lib -lSDL2 -lSDL2_image -I/usr/local/include/SDL2 -D_THREAD_SAFE
